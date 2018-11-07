a2d94d371f3d53e4e0ed6bc372b70ee2073a2a56
Danilo Paoletti <danilo.paoletti@regione.marche.it>
2018-11-07 16:07:32 +0100

    inserimento menu

--- app.js
+++ app.js
@@ -1,32 +1,72 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
 const url = require('url')
+const electron = require('electron')
 
 let window = null
 
-// Wait until the app is ready
+// aspetta fino a che l'app non è pronta
 app.once('ready', () => {
-  // Create a new window
+  // Creo la finestra iniziale
   window = new BrowserWindow({
-    // Set the initial width to 800px
-    width: 800,
-    // Set the initial height to 600px
-    height: 600,
-    // Set the default background color of the window to match the CSS
-    // background color of the page, this prevents any white flickering
+    // Imposto l'altezza iniziale a 1000px
+    width: 1000,
+    // Imposto la larghezza iniziale a 800px
+    height: 800,
+    // Imposto il colore di sfondo
     backgroundColor: "#D6D8DC",
-    // Don't show the window until it's ready, this prevents any white flickering
+    // non mostra la finestra fino a che non è pronta
     show: false
   })
 
-  // Load a URL in the window to the local index.html path
+
+  //Creo il menù
+  var menu = Menu.buildFromTemplate([
+    {
+        label: 'File',
+        submenu: [
+            {label:'Importa file'},
+            {label:'Salva'},
+            {label:'Salva con nome'},
+        ]
+    },
+    { 
+      label: 'Opzioni',
+      submenu: [
+        {label: 'Rilancia',
+      click(){
+        app.relaunch()
+        app.quit()
+      }},
+      ]
+    },
+    { 
+      label: 'Info',
+      submenu: [
+        {label: 'Chi siamo',
+      click: function(){
+        electron.shell.openExternal('http://www.odcec.an.it/')
+      }},
+      ]
+    },
+    {
+      label: 'Esci',
+        click(){
+          app.quit()
+      }
+    }
+])
+Menu.setApplicationMenu(menu); 
+
+
+  // Carica un URL nella finestra al locale index.html
   window.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-  // Show window when page is ready
+  // Mostra finestra quando la pagina è pronta
   window.once('ready-to-show', () => {
     window.show()
   })
