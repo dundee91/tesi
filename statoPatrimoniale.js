f4f186531f203598c5489c4ff005c957af4429ec
danilopaoletti91@gmail.com <danilopaoletti91@gmail.com>
2018-11-26 17:25:02 +0100

    modifiche somme e input form

--- statoPatrimoniale.js
+++ statoPatrimoniale.js
@@ -1,10 +1,52 @@
+var validate = function(e) {
+    var t = e.value;
+    e.value = (t.indexOf(".") >= 0) ? (t.substr(0, t.indexOf(".")) + t.substr(t.indexOf("."), 3)) : t;
+  }
+  /* negativi
+  var charCode = (e.which) ? e.which : e.keyCode;
+  if (charCode != 46 && charCode > 31 
+     && (charCode < 48 || charCode > 57))
+  return false;
+  */
+
+function somme(){
+
+    sommaImmobilizzazioniImmateriali()
+    sommaImmobilizzazioniMateriali()
+    sommaImmobilizzazioniFinanziarie()
+
+    var totaleImmobilizzazioniImmateriali = document.getElementById('totaleImmobilizzazioniImmateriali').value
+    var totaleImmobilizzazioniMateriali = document.getElementById('totaleImmobilizzazioniMateriali').value
+    var totaleImmobilizzazioniFinanziarie = document.getElementById('totaleImmobilizzazioniFinanziarie').value
+    document.getElementById('totaleImmobilizzazioni').value = totaleImmobilizzazioniImmateriali + totaleImmobilizzazioniMateriali + totaleImmobilizzazioniFinanziarie
+}
 
 function sommaImmobilizzazioniImmateriali() {
     var arr = document.getElementsByName('immobilizzazioniImmateriali');
     var tot = 0;
     for (var i = 0; i < arr.length; i++) {
-        if (parseInt(arr[i].value))
-            tot += parseInt(arr[i].value);
+        if (parseFloat(arr[i].value))
+            tot += parseFloat(arr[i].value);
     }
-    document.getElementById('totaleImmobilizzazioniImmateriali').value = tot;
+    document.getElementById('totaleImmobilizzazioniImmateriali').value = tot.toFixed(2);
+}
+
+function sommaImmobilizzazioniMateriali(){
+    var arr = document.getElementsByName('immobilizzazioniMateriali');
+    var tot = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (parseFloat(arr[i].value))
+            tot += parseFloat(arr[i].value);
+    }
+    document.getElementById('totaleImmobilizzazioniMateriali').value = tot.toFixed(2);
+}
+
+function sommaImmobilizzazioniFinanziarie(){
+    var arr = document.getElementsByName('immobilizzazioniFinanziarie');
+    var tot = 0;
+    for (var i = 0; i < arr.length; i++) {
+        if (parseFloat(arr[i].value))
+            tot += parseFloat(arr[i].value);
+    }
+    document.getElementById('totaleImmobilizzazioniFinanziarie').value = tot.toFixed(2);
 }
